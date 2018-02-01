ddc2af4bd74522c7c66d82b41422a6e532219841
Rafal Prusak <rafal.prusak@gmail.com>
2018-02-01 17:27:40 -0500

    Pobieranie danych dla mapy 3d.

--- 3d/3d.js
+++ 3d/3d.js
@@ -15,60 +15,6 @@ function createMap() {
   map.addMapTiles('https://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png');
   map.addGeoJSONTiles('https://{s}.data.osmbuildings.org/0.2/ph2apjye/tile/{z}/{x}/{y}.json', { fixedZoom: 15 });
 
-  var geojson = {
-    type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      properties: {
-        color: '#00ff00',
-        roofColor: '#00ff00',
-        height: 50,
-        minHeight: 0
-      },
-      geometry: {
-        type: 'Polygon',
-        coordinates: [
-          [
-            [19.93000, 50.06000],
-            [19.93010, 50.06000],
-            [19.93010, 50.06010],
-            [19.93000, 50.06010],
-            [19.93000, 50.06000]
-          ]
-        ]
-      }
-    }]
-  };
-
-  map.addGeoJSON(geojson);
-
-  var geojson2 = {
-    type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      properties: {
-        color: '#00ff00',
-        roofColor: '#00ff00',
-        height: 100,
-        minHeight: 0
-      },
-      geometry: {
-        type: 'Polygon',
-        coordinates: [
-          [
-            [19.94000, 50.05000],
-            [19.94010, 50.05000],
-            [19.94010, 50.05010],
-            [19.94000, 50.05010],
-            [19.94000, 50.05000]
-          ]
-        ]
-      }
-    }]
-  };
-
-  map.addGeoJSON(geojson2);
-
   map.on('pointermove', function(e) {
     map.getTarget(e.detail.x, e.detail.y, function(id) {
       if (id) {
@@ -80,8 +26,61 @@ function createMap() {
   });
 }
 
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+}
+
+
+function getPointsFromApi() {
+  $.get("/api/points", function(data, status){
+   shuffle(data);
+    for (var i=0; i < 100; i++) {
+      addPoint(data[i].latitude, data[i].longitude, data[i].height);
+    }
+
+    console.log(status);
+  });
+}
+
+function addPoint(long, lat, height) {
+  var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      properties: {
+        color: '#00ff00',
+        roofColor: '#00ff00',
+        height: height,
+        minHeight: 0
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [long - 0.0001, lat  - 0.0001],
+            [long + 0.0001, lat  - 0.0001],
+            [long + 0.0001, lat  + 0.0001],
+            [long - 0.0001, lat  + 0.0001],
+            [long - 0.0001, lat  - 0.0001]
+          ]
+        ]
+      }
+    }]
+  };
+
+  map.addGeoJSON(geojson);
+}
+
+
 $( document ).ready(function() {
   createMap();
+  getPointsFromApi();
 });
 
 
